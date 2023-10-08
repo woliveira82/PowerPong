49d711517a3e08fde6d5d68a6d1416b68f399374
Weiner Oliveira <woliveira82@gmail.com>
2023-10-08 17:19:32 +0100

    fix: credits scene

--- src/control/MainMenu.gd
+++ src/control/MainMenu.gd
@@ -3,17 +3,16 @@ extends CanvasLayer
 
 func _on_FastMatch_pressed():
 	GameData._set_random_opponent()
-	SceneDirector.change_to("res://src/scenes/game/GameField.tscn")
+	SceneDirector.change_to("res://src/game/GameField.tscn")
 
 
 func _on_Championship_pressed():
-	# SceneDirector.change_to("res://src/scenes/control/ChampionshipSplash.tscn")
-	pass
+	SceneDirector.change_to("res://src/control/ChampionshipSplash.tscn")
 
 
 func _on_Options_pressed():
-	SceneDirector.change_to("res://src/scenes/control/Options.tscn")
+	SceneDirector.change_to("res://src/control/Options.tscn")
 
 
 func _on_Credits_pressed():
-	SceneDirector.change_to("res://src/scenes/control/Credits.tscn")
+	SceneDirector.change_to("res://src/control/Credits.tscn")
