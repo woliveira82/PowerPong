837615a5109db2e81d9e99a33ee5aeeef41d8205
Weiner Oliveira <woliveira82@gmail.com>
2023-10-08 15:25:16 +0100

    fix: main menu

--- src/control/SplashScreen.gd
+++ src/control/SplashScreen.gd
@@ -7,4 +7,4 @@ func _ready():
 
 func _to_main_menu():
 	# SceneDirector.change_to("res://src/scenes/control/Playoffs.tscn")
-	SceneDirector.change_to("res://src/scenes/control/MainMenu.tscn")
+	SceneDirector.change_to("res://src/control/MainMenu.tscn")
