3665e42960aea4482655c6af87fc43ce145dcc67
Weiner Oliveira <woliveira82@gmail.com>
2023-10-29 00:26:42 +0100

    refact: remove play button in match box

--- src/championship/match_background.gd
+++ src/championship/match_background.gd
@@ -1,7 +1,5 @@
 extends ColorRect
 
-@onready var _match_container := $MatchContainer
-@onready var _play_container := $PlayContainer
 @onready var _player1_name := $MatchContainer/PlayerContainer1/PlayerName
 @onready var _p1_score1 := $MatchContainer/PlayerContainer1/Score1
 @onready var _p1_score2 := $MatchContainer/PlayerContainer1/Score2
@@ -25,27 +23,3 @@ func set_match(data: Dictionary):
 	self._p2_score3.text = str(data.player2_score[2])
 	if data.human_player >= 0:
 		self._player = data.human_player
-
-
-func _show_play_container():
-	self._match_container.hide()
-	self._play_container.show()
-
-
-func _show_match_container():
-	self._play_container.hide()
-	self._match_container.show()
-
-
-func _on_gui_input(event):
-	if event is InputEventMouseButton:
-		if event.pressed and self._player != 0:
-			self._show_play_container()
-
-
-func _on_cancel_button_pressed():
-	self._show_match_container()
-
-
-func _on_play_button_pressed():
-	SceneDirector.change_scene_to_match()
