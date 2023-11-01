a0e9e2386164dddce7cc6f653f599fd26dac45e5
Weiner Oliveira <woliveira82@gmail.com>
2023-11-01 10:52:25 +0000

    fix: opponent stats in game

--- src/game/Opponent.gd
+++ src/game/Opponent.gd
@@ -13,14 +13,16 @@ static var type := {
 	"JACK": [FASTER, BIGGER, TATICAL]
 }
 
+const MIDDLE_FIELD := Vector2(135.0, 240.0)
+
 const BIGGER_SIZE := Vector2(8.0, 78.0)
 const BIGGER_SPRITE_X_SCALE := 2.438
-const FASTER_SPEED := 300
+const FASTER_SPEED := 270
 
 var ball: Node2D
-var _speed = 200
-var _tolerance = 5
-var _go_middle = false
+var _speed := 180
+var _tolerance := 5
+var _go_middle := false
 var current_type: String
 
 @onready var _sprite := $Sprite2D
@@ -31,14 +33,16 @@ func set_opponent(new_type: String):
 	current_type = new_type
 	var status = Opponent.type[new_type]
 	if FASTER in status:
-		_speed = FASTER_SPEED
+		self._speed = FASTER_SPEED
 	
 	if BIGGER in status:
 		self._sprite.scale.x = BIGGER_SPRITE_X_SCALE
 		self._collision_shape.shape.set("size", BIGGER_SIZE)
 	
 	if TATICAL in status:
-		_go_middle = true
+		self._go_middle = true
+	
+	self._tolerance = randi_range(5, 15)
 
 
 func _physics_process(_delta):
@@ -47,11 +51,13 @@ func _physics_process(_delta):
 
 
 func _get_direction():
-	if self._go_middle:
-		if ball.position.y > 960:
-			return 1 if position.x < 540 else -1
+	if self._go_middle and ball.position.y > MIDDLE_FIELD.y:
+		if abs(self.position.x - MIDDLE_FIELD.x) < self._tolerance:
+			return 0
 		
-	if abs(ball.position.x - position.x) > _tolerance:
+		return 1 if position.x < MIDDLE_FIELD.x else -1
+		
+	if abs(ball.position.x - position.x) > self._tolerance:
 		return 1 if ball.position.x > position.x else -1
 	
 	return 0
